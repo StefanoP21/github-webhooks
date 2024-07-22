d9e122ecb2ae4f3bac9748828cd55454959237b7
Stefano <stefanop21@outlook.es>
2024-07-21 21:49:46 -0500

    feat: colocar tipado estrcito en las respuestas

---
+++ src/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { GitHubIssuePayload } from './github-issue.interface';
+export { GitHubStartPayload } from './github-start.interface';
--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -4,10 +4,8 @@ export class GitHubController {
     constructor() {}
 
     webhooksHandler(req: Request, res: Response) {
-        const githubEvent = req.headers['x-github-event'];
+        const githubEvent = req.headers['x-github-event'] ?? 'unknown';
         const payload = req.body;
-
-        console.log({ githubEvent });
 
         res.status(202).send('Webhook received');
     }
