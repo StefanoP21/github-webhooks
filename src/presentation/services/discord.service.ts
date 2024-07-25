b84954594e983de4145085fdbbd4a33addc183cd
Stefano <stefanop21@outlook.es>
2024-07-25 15:57:36 -0500

    update: enviar gif animado a discord

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -8,6 +8,13 @@ export class DiscordService {
     async notify(message: string) {
         const body = {
             content: message,
+            embeds: [
+                {
+                    image: {
+                        url: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmh1NjF2N2FqaGY2ejl5NXB3bzZibTVudHF4dWhlNjdnbDZmbWJvcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26u4nJPf0JtQPdStq/giphy.gif',
+                    },
+                },
+            ],
         };
 
         const response = await fetch(this.discordWebhookUrl, {
